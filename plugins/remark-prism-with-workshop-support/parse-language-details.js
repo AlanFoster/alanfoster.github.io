96c25e85180b424d1cd0ded318d6eff717a7629b
alanfoster <alan@alanfoster.me>
2022-11-19 03:41:27 +0000

    Update prism for rust highlighting support

--- plugins/remark-prism-with-workshop-support/parse-language-details.js
+++ plugins/remark-prism-with-workshop-support/parse-language-details.js
@@ -3,12 +3,13 @@ const rangeParser = require(`parse-numeric-range`);
 module.exports = (node) => {
   const language = (node.lang || "none").toLowerCase();
   const parsedConfig = JSON.parse(node.meta || "{}");
+  const highlightLines = rangeParser(parsedConfig.highlight || "").filter(
+    (n) => n > 0
+  );
 
   return {
     language: language,
     ...parsedConfig,
-    highlightLines: rangeParser
-      .parse(parsedConfig.highlight || "")
-      .filter((n) => n > 0),
+    highlightLines: highlightLines,
   };
 };
